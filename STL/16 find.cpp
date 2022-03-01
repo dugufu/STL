3f19d427f8a370a89056399d30fa528235077704
ccr <cchunren1997@gmail.com>
2022-03-01 20:47:07 +0800

    #3 conflict

--- STL/16 find.cpp
+++ STL/16 find.cpp
@@ -24,7 +24,7 @@ void test01()
 	}
 
 	vector<int>vTarget;
-	vTarget.resize(v.size() + v2.size());
+	vTarget.resize(v.size());
 	//no conflict needed
 	merge(v.begin(), v.end(), v2.begin(), v2.end(), vTarget.begin());
 	for_each(vTarget.begin(), vTarget.end(), myPrint);//abc 1234
