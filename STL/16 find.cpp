71e387138cba5dabdbded6a3eeaac42a1a63a1dd
dugufu <cchunren1997@gmail.com>
2023-04-13 23:00:57 +0800

    update

--- STL/16 find.cpp
+++ STL/16 find.cpp
@@ -1,41 +1,42 @@
-#include <iostream>
-using namespace std;
-#include <functional>
-#include <vector>
-#include <algorithm>
+// #include <iostream>
+// using namespace std;
+// #include <functional>
+// #include <vector>
+// #include <algorithm>
 
-void myPrint(int val)
-{
-	cout << val << endl;
-}
+// void myPrint(int val)
+// {
+// 	cout << val << endl;
+// }
 
-void test01()
-{
-	vector<int>v;
-	vector<int>v2;
+// void test01()
+// {
+// 	vector<int> v;
+// 	vector<int> v2;
 
-	for (int i = 0 ; i < 10 ; i+=1)
-	{
-		v.push_back(i);
-	}
-	for (int i = 0; i < 10; i +=2)
-	{
-		v2.push_back(i);
-	}
+// 	for (int i = 0; i < 10; i += 1)
+// 	{
+// 		v.push_back(i);
+// 		myPrint(i);
+// 	}
+// 	for (int i = 0; i < 10; i += 2)
+// 	{
+// 		v2.push_back(i);
+// 	}
 
-	for (int i = 0; i < 10; i += 2)
-	{
-		v2.push_back(i);
-	}
-// 	vector<int>vTarget;
-// 	vTarget.resize(v.size() + v2.size());
-// 	//no conflict needed
-// 	merge(v.begin(), v.end(), v2.begin(), v2.end(), vTarget.begin());
-// 	for_each(vTarget.begin(), vTarget.end(), myPrint);//abc 1234
-	//this is text
-	
-}
+// 	for (int i = 0; i < 10; i += 2)
+// 	{
+// 		v2.push_back(i);
+// 	}
+// 	// 	vector<int>vTarget;
+// 	// 	vTarget.resize(v.size() + v2.size());
+// 	// 	//no conflict needed
+// 	// 	merge(v.begin(), v.end(), v2.begin(), v2.end(), vTarget.begin());
+// 	// 	for_each(vTarget.begin(), vTarget.end(), myPrint);//abc 1234
+// 	// this is text
+// }
 
-int main() {
-	test01();
-}
+// int main()
+// {
+// 	test01();
+// }
