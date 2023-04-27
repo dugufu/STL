07179ceec565d35b5ddda5fe652290474ccb95bf
dugufu <cchunren1997@gmail.com>
2023-04-27 22:47:40 +0800

    update 14,15

--- STL/14 for_each.cpp
+++ STL/14 for_each.cpp
@@ -1,40 +1,40 @@
-// #include <iostream>
-// using namespace std;
-// #include <functional>
-// #include <vector>
-// #include <algorithm>
-// 
-// void printVector(int val)
-// {
-// 	cout << val << " ";
-// }
-// 
-// class printVectorClass
-// {
-// public:
-// 	void operator()(int val)
-// 	{
-// 		cout << val << " ";
-// 	}
-// };
-// 
-// void test01()
-// {
-// 	vector<int>v;
-// 	for (int i = 0 ;i<10 ;i++)
-// 	{
-// 		v.push_back(i);
-// 	}
-// 	
-// 
-// 	cout << "Function :" << endl;
-// 	for_each(v.begin(), v.end(), printVector);
-// 	cout << endl;
-// 	cout << "Function Object :" << endl;
-// 	for_each(v.begin(), v.end(), printVectorClass());
-// 	cout << endl;
-// }
-// 
-// int main() {
-// 	test01();
-// }
+#include <iostream>
+using namespace std;
+#include <functional>
+#include <vector>
+#include <algorithm>
+
+void printVector(int val)
+{
+    cout << val << " ";
+}
+
+class printVectorClass
+{
+public:
+    void operator()(int val)
+    {
+        cout << val << " ";
+    }
+};
+
+void test01()
+{
+    vector<int> v;
+    for (int i = 0; i < 10; i++)
+    {
+        v.push_back(i);
+    }
+
+    cout << "Function :" << endl;
+    for_each(v.begin(), v.end(), printVector);
+    cout << endl;
+    cout << "Function Object :" << endl;
+    for_each(v.begin(), v.end(), printVectorClass());
+    cout << endl;
+}
+
+int main()
+{
+    test01();
+}
